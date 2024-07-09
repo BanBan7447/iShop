0f9a75aeeb1528b3d710293fa3a84042bf9f981d
BanBan <pn93948848@gmail.com>
2024-07-09 21:58:44 +0700

    Update giao diện theo lời cô sáng nay

--- app/src/main/java/com/example/ishop/Type_Customers/Page_Fix_Profile.java
+++ app/src/main/java/com/example/ishop/Type_Customers/Page_Fix_Profile.java
@@ -2,15 +2,19 @@ package com.example.ishop.Type_Customers;
 
 import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.example.ishop.R;
 
-public class Page_Detail_Product extends AppCompatActivity {
+public class Page_Fix_Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ui_page_buy_product);
+        setContentView(R.layout.ui_page_fix_profile);
     }
 }
--- app/src/main/java/com/example/ishop/Type_Manager/Page_Manage_Detail_Product.java
+++ app/src/main/java/com/example/ishop/Type_Manager/Page_Manage_Detail_Product.java
@@ -2,11 +2,7 @@ package com.example.ishop.Type_Manager;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.ishop.R;
 
@@ -15,6 +11,6 @@ public class Page_Manage_Detail_Product extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ui_page_manage_detail_product);
+        setContentView(R.layout.ui_page_detail_manage_product);
     }
 }
