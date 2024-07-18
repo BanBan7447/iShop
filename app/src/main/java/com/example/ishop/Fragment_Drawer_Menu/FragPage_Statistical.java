197d63a2a6fac2dcbb828517c5e1a97b5617556e
Sallysayhiii <kate1225kate@gmail.com>
2024-07-18 12:57:47 +0700

    Xong giao diện thống kê

--- app/src/main/java/com/example/ishop/Fragment_Drawer_Menu/FragPage_Statistical.java
+++ app/src/main/java/com/example/ishop/Fragment_Drawer_Menu/FragPage_Statistical.java
@@ -23,6 +23,38 @@ public class FragPage_Statistical extends Fragment {
     public View onCreateView(@NonNull LayoutInflater layoutInflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View view = layoutInflater.inflate(R.layout.ui_frag_page_statistical, container, false);
 
+        RelativeLayout MovePage_Total_Revenue = view.findViewById(R.id.MovePage_Revenue_Statistical);
+        MovePage_Total_Revenue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), Page_Statistical_Revenue.class));
+            }
+        });
+
+        RelativeLayout MovePage_Total_Customer_SignUp = view.findViewById(R.id.MovePage_Customer_Statistical);
+        MovePage_Total_Customer_SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), Page_Statistical_Customer.class));
+            }
+        });
+
+        RelativeLayout MovePage_Total_Customer_Buy = view.findViewById(R.id.MovePage_Product_Statistical);
+        MovePage_Total_Customer_Buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), Page_Statistical_Product.class));
+            }
+        });
+
+        RelativeLayout MovePage_iPhone_Orders = view.findViewById(R.id.MovePage_Orders_Statistical);
+        MovePage_iPhone_Orders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), Page_Statistical_Purchase.class));
+            }
+        });
+
         return view;
     }
 }
